c2d10274c87dd023d9189c1535abcfa568cb55dc
Semen Alekseev <s@qd2.ru>
2020-05-05 22:54:42 +0300

    add file cache(CacheItemPoolInterface )

---
+++ src/Libs/Settings.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace Sura\Libs;
+
+
+class Settings
+{
+ public static function loadsettings()
+ {
+     return include __DIR__.'/../../../../../config/config.php';
+     return true;
+ }
+}
