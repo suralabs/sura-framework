c9a1c5fbaf89cdfd545ed6fe7a53779fcfcdecf9
Semen Alekseev <semyon492@ya.ru>
2021-02-28 21:36:03 +0300

    Update

--- src/Libs/Module.php
+++ src/Libs/Module.php
@@ -11,6 +11,36 @@ use Sura\Contracts\ModuleInterface;
  */
 class Module implements ModuleInterface
 {
+    private static ?\Sura\Database\Connection $database = null;
+
+    /**
+     * Model constructor.
+     *
+     * Получение экземпляра класса.
+     * Если он уже существует, то возвращается, если его не было,
+     * то создаётся и возвращается (паттерн Singleton)
+     */
+    public function __construct()
+    {
+        self::$database = self::getDB();
+    }
+
+    public static function getDB(): \Sura\Database\Connection
+    {
+        if (self::$database == null) {
+            $config = Settings::load();
+
+            $dsn = 'mysql:host=' . $config['dbhost'] . ';dbname=' . $config['dbname'];
+            $user = $config['dbuser'];
+            $password = $config['dbpass'];
+
+//            $database = new \Sura\Database\Connection($dsn, $user, $password); // the same arguments as uses PDO
+
+            self::$database = new \Sura\Database\Connection($dsn, $user, $password);
+        }
+        return self::$database;
+    }
+
 	/**
 	 * @return string|array|null
 	 */
